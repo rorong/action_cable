af7f8906326cd9a791037be634210f94bfc5dfb6
rorong <rordev@ongraph.com>
2019-06-20 17:13:51 +0530

    rspec for chats controller

--- spec/controllers/chats_controller_spec.rb
+++ spec/controllers/chats_controller_spec.rb
@@ -7,24 +7,16 @@ RSpec.describe ChatsController, type: :controller do
   let(:valid_attributes) { build(:chat).as_json }
   let(:invalid_attributes) {}
 
-  describe 'GET #new' do
-    it 'assigns a new user as @user' do
-      get :new
-      expect(assigns(:user)).to be_a_new(User)
-      expect(response).to be_success
-    end
-  end
-
   describe 'POST #create' do
     context 'with valid params' do
-      it 'creates a new User' do
-        expect { post :create, params: { user: valid_attributes } }
+      it 'creates a new Chat' do
+        expect { post :create, params: { chat: valid_attributes } }
       end
     end
 
     context 'with invalid params' do
-      it 'Should not create user with invalid params' do
-        expect { post :create, params: { user: invalid_attributes } }
+      it 'Should not create chat with invalid params' do
+        expect { post :create, params: { chat: invalid_attributes } }
       end
     end
   end
